d528a854d4f25b8058d030ac78ad7c8c47786326
Hadar Rottenberg <hadar@gooddollar.org>
2020-06-14 21:42:17 +0300

    add: upgrade gun

--- config.js
+++ config.js
@@ -6,63 +6,58 @@ const conf = convict({
   name: {
     format: String,
     default: "goodgun",
-    env: "GUN_NAME"
+    env: "GUN_NAME",
   },
   peers: {
     format: Array,
     default: [],
-    env: "GUN_PEERS"
+    env: "GUN_PEERS",
   },
   gunOpts: {
     format: Object,
     description: "override or add gun opts",
-    default: {
-      radisk: false,
-      axe: true,
-      multicast: false,
-      localStorage: false
-    },
-    env: "GUN_OPTS"
+    default: {},
+    env: "GUN_OPTS",
   },
   mongoUrl: {
     format: String,
     default: undefined,
-    env: "MONGO_URL"
+    env: "MONGO_URL",
   },
   mongoPort: {
     format: "port",
     default: "27017",
-    env: "MONGO_PORT"
+    env: "MONGO_PORT",
   },
   mongoQuery: {
     format: String,
     default: "",
-    env: "MONGO_QUERY"
+    env: "MONGO_QUERY",
   },
   mongoDB: {
     format: String,
     default: "test",
-    env: "MONGO_DB"
+    env: "MONGO_DB",
   },
   mongoCollection: {
     format: String,
     default: "gun_mongo_key",
-    env: "MONGO_COLLECTION"
+    env: "MONGO_COLLECTION",
   },
   gunPublicS3: {
     key: {
       format: "*",
-      default: undefined
+      default: undefined,
     },
     secret: {
       format: "*",
-      default: undefined
+      default: undefined,
     },
     bucket: {
       format: "*",
-      default: undefined
-    }
-  }
+      default: undefined,
+    },
+  },
 });
 
 const publicS3 = process.env.GUN_PUBLIC_S3;
